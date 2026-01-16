627af0adcc604bd541d1ebc22dc1f12620563741
Alice39s <alice39s@xmsl.dev>
2026-01-16 23:35:01 +0900

    style(test): Fix prettier formatting

--- packages/react/src/tests/gen-flags-data.test.ts
+++ packages/react/src/tests/gen-flags-data.test.ts
@@ -8,14 +8,21 @@ describe('flag metadata coverage', () => {
     const countryNames = getCountryNameMap()
     const allNames = {
       ...countryNames,
-      ...Object.keys(SUBDIVISION_NAMES).reduce((acc, key) => {
-        acc[key] = `${SUBDIVISION_NAMES[key as keyof typeof SUBDIVISION_NAMES].country} - ${SUBDIVISION_NAMES[key as keyof typeof SUBDIVISION_NAMES].region}`
-        return acc
-      }, {} as Record<string, string>),
-      ...Object.keys(COUNTRY_NAMES).reduce((acc, key) => {
-        acc[key] = COUNTRY_NAMES[key as keyof typeof COUNTRY_NAMES]
-        return acc
-      }, {} as Record<string, string>),
+      ...Object.keys(SUBDIVISION_NAMES).reduce(
+        (acc, key) => {
+          acc[key] =
+            `${SUBDIVISION_NAMES[key as keyof typeof SUBDIVISION_NAMES].country} - ${SUBDIVISION_NAMES[key as keyof typeof SUBDIVISION_NAMES].region}`
+          return acc
+        },
+        {} as Record<string, string>
+      ),
+      ...Object.keys(COUNTRY_NAMES).reduce(
+        (acc, key) => {
+          acc[key] = COUNTRY_NAMES[key as keyof typeof COUNTRY_NAMES]
+          return acc
+        },
+        {} as Record<string, string>
+      ),
     }
 
     const flagsDir = resolve(__dirname, '../../generated/flags')
