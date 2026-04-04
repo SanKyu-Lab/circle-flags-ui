13d60a8c40384a96c4a0245f21e8481150144fe3
Alice39s <alice39s@xmsl.dev>
2026-04-04 14:16:09 +0900

    fix(eslint): Remove eslint-plugin-react, incompatible with ESLint 10

--- packages/react/eslint.config.js
+++ packages/react/eslint.config.js
@@ -1,7 +1,6 @@
 import js from '@eslint/js'
 import typescript from '@typescript-eslint/eslint-plugin'
 import typescriptParser from '@typescript-eslint/parser'
-import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import prettier from 'eslint-plugin-prettier'
 import prettierConfig from 'eslint-config-prettier'
@@ -30,23 +29,14 @@ export default [
     },
     plugins: {
       '@typescript-eslint': typescript,
-      react: react,
       'react-hooks': reactHooks,
       prettier: prettier,
     },
     rules: {
       ...typescript.configs.recommended.rules,
-      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
       'prettier/prettier': 'warn',
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
     },
   },
 ]
